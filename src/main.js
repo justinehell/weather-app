72f42a7122455118a1af46c41feedca4309d63fa
Justine <hell.justine@gmail.com>
2021-02-07 16:24:21 +0100

    part 1 - set up vuex store

--- src/main.js
+++ src/main.js
@@ -1,6 +1,7 @@
 import Vue from "vue";
 import App from "./App.vue";
 import BaseWeatherIcon from "./components/Base/BaseWeatherIcon.vue";
+import store from "./store";
 
 // globally register the BaseIcon Component
 Vue.component("BaseWeatherIcon", BaseWeatherIcon);
@@ -9,4 +10,5 @@ Vue.config.productionTip = false;
 
 new Vue({
   render: (h) => h(App),
+  store,
 }).$mount("#app");
