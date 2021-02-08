0ba4b39794b6ee39f7de8dcedb2866cc24ad8fb9
Justine <hell.justine@gmail.com>
2021-02-08 18:56:53 +0100

    refactor WeatherServices & MainCard component

--- src/components/MainCard.vue
+++ src/components/MainCard.vue
@@ -7,7 +7,7 @@
       <CurrentWeatherData :currentWeatherData="search.current" class="mb-4" />
       <div class="d-flex justify-content-between mb-4">
         <DayThumbnail
-          v-for="(el, index) in [currentDay, ...getNextFiveDaysWeatherData]"
+          v-for="(el, index) in search.forecast"
           :key="index"
           :icon="el.icon"
           :day="el.dt"
@@ -43,85 +43,13 @@
 <script>
 import DayThumbnail from "./DayThumbnail";
 import CurrentWeatherData from "./CurrentWeatherData";
-import { getDayString, getDayInt } from "../utils";
-import _ from "lodash";
 
 export default {
   components: {
     DayThumbnail,
     CurrentWeatherData,
   },
-  methods: {
-    getArrayOfIcons(arr, index) {
-      return arr[index].map((data) => data.weather[0].icon);
-    },
-    findMostFrequentItem(arr) {
-      let result = _.head(
-        _(arr)
-          .countBy()
-          .entries()
-          .maxBy(_.last)
-      );
-      return result;
-    },
-    getArrayOfTemp(arr, index, temp) {
-      return arr[index].map((data) => data.main[temp]);
-    },
-    getForecastDataPerDay(array, index) {
-      let dataPerDay = {
-        dt: getDayString(array[index][0].dt, "short"),
-        maxT: Math.ceil(
-          Math.max(...this.getArrayOfTemp(array, index, "temp_max"))
-        ),
-        minT: Math.floor(
-          Math.min(...this.getArrayOfTemp(array, index, "temp_min"))
-        ),
-        icon: this.findMostFrequentItem(this.getArrayOfIcons(array, index)),
-      };
-      return dataPerDay;
-    },
-  },
   computed: {
-    currentDay() {
-      if (!this.search) return null;
-      return {
-        icon: this.search.current.weather[0].icon,
-        dt: getDayString(this.search.current.dt, "short"),
-        minT: Math.floor(this.search.current.main.temp_min),
-        maxT: Math.ceil(this.search.current.main.temp_max),
-      };
-    },
-    getNextFiveDaysWeatherData() {
-      if (!this.search) return [];
-      let currentDay = getDayInt(this.search.current.dt); // 0 for Sunday and 6 for Saturday
-      let copyForecastData = [...this.search.forecast.list]; // copy data to avoid mutating
-      let nextFiveDaysWeatherDataArray = copyForecastData.filter(
-        (data) => getDayInt(data.dt) !== currentDay
-      );
-      // nextFiveDaysWeatherDataArray
-      // contains all the weather data for the next 5 days (without data for the current day, removed from filter function)
-      // from this big array of objects, we need to separate the data related to each day
-      let DATA_PER_DAY = 8; // for each day, we have 8 objects representing the data
-      let chunkArrays = _.chunk(nextFiveDaysWeatherDataArray, DATA_PER_DAY);
-
-      // For each day, we have 8 data objects. Now, we need to get the
-      // date time, max temp, min temp & most frequent icon to display global weather info
-      let forecastData = [
-        this.getForecastDataPerDay(chunkArrays, 0),
-        this.getForecastDataPerDay(chunkArrays, 1),
-        this.getForecastDataPerDay(chunkArrays, 2),
-        this.getForecastDataPerDay(chunkArrays, 3),
-      ];
-
-      // handle a particular case where the filter function (l.85) removes the data for the current day
-      // Problem : when it's between midnight and 3:00 am, a whole chunk of data corresponding to the current day is removed
-      // So we only have 4 chunks left of 8 datas each (so data.length <= 32). We want to display 5 chunks of data when
-      // the forecast data.length > 32
-      if (nextFiveDaysWeatherDataArray.length > 32) {
-        forecastData.push(this.getForecastDataPerDay(chunkArrays, 4));
-      }
-      return forecastData;
-    },
     search() {
       return this.$store.state.search;
     },
--- src/services/WeatherService.js
+++ src/services/WeatherService.js
@@ -1,4 +1,7 @@
 import axios from "axios";
+import _ from "lodash";
+
+import { getDayInt, getDayString } from "../utils";
 
 const apiOpenWeather = axios.create({
   baseURL: "https://api.openweathermap.org/data/2.5/",
@@ -12,14 +15,73 @@ const apiOpenWeather = axios.create({
 const API_TOKEN = process.env.VUE_APP_TOKEN;
 
 export default {
-  getWeather(city = "lyon") {
-    return apiOpenWeather.get(
-      "weather?q=" + city + "&lang=fr&units=metric&appid=" + API_TOKEN
+  getWeather(city) {
+    return new Promise((resolve, reject) =>
+      apiOpenWeather
+        .get("weather?q=" + city + "&lang=fr&units=metric&appid=" + API_TOKEN)
+        .then((response) => resolve(response.data))
+        .catch((error) => reject(error))
     );
   },
-  getNextFiveDaysWeather(city = "lyon") {
-    return apiOpenWeather.get(
-      "forecast?q=" + city + "&lang=fr&units=metric&appid=" + API_TOKEN
+  getNextFiveDaysWeather(city) {
+    return new Promise((resolve, reject) =>
+      apiOpenWeather
+        .get("forecast?q=" + city + "&lang=fr&units=metric&appid=" + API_TOKEN)
+        .then((response) => {
+          resolve(getNextFiveDaysWeatherData(response.data));
+        })
+        .catch((error) => reject(error))
     );
   },
+};
+
+export const getArrayOfIcons = (arr, index) => {
+  return arr[index].map((data) => data.weather[0].icon);
+};
+
+export const findMostFrequentItem = (arr) => {
+  let result = _.head(
+    _(arr)
+      .countBy()
+      .entries()
+      .maxBy(_.last)
+  );
+  return result;
+};
+
+export const getArrayOfTemp = (arr, index, temp) => {
+  return arr[index].map((data) => data.main[temp]);
+};
+
+export const getForecastDataPerDay = (array, index) => {
+  let dataPerDay = {
+    dt: getDayString(array[index][0].dt, "short"),
+    maxT: Math.ceil(Math.max(...getArrayOfTemp(array, index, "temp_max"))),
+    minT: Math.floor(Math.min(...getArrayOfTemp(array, index, "temp_min"))),
+    icon: findMostFrequentItem(getArrayOfIcons(array, index)),
+  };
+  return dataPerDay;
+};
+
+export const getNextFiveDaysWeatherData = (data) => {
+  if (!data.list) return [];
+  let currentDay = getDayInt(new Date()); // 0 for Sunday and 6 for Saturday
+  let forcastWeatherDataArray = [...data.list]; // copy data to avoid mutating
+  let nextFiveDaysWeatherDataArray = forcastWeatherDataArray.filter(
+    (data) => getDayInt(data.dt) !== currentDay
+  );
+  // contains all the weather data for the next 5 days in an array of object
+  // we need to separate the data related to each day
+  let DATA_PER_DAY = 8; // 8 objects data for each day
+  let chunkArrays = _.chunk(nextFiveDaysWeatherDataArray, DATA_PER_DAY);
+
+  // we only need the date, max temp, min temp & most frequent icon
+  let forecastData = [
+    getForecastDataPerDay(chunkArrays, 0),
+    getForecastDataPerDay(chunkArrays, 1),
+    getForecastDataPerDay(chunkArrays, 2),
+    getForecastDataPerDay(chunkArrays, 3),
+    getForecastDataPerDay(chunkArrays, 4),
+  ];
+  return forecastData;
 };
